2ffa43b09ff9961fad7a368619679f558eb4bb42
Mayank Grover <groverm@codeaurora.org>
2020-11-05 16:47:13 +0530

    BACKPORT: ANDROID: modules: cfi cleanup for module load failure
    Cleanup cfi shadow for failure in module loading,
    to avoid causing warnings.
    
    Bug: 172542186
    Change-Id: I1de7ffa7d884c8e46891b8bbc8196ec0d2cef0d6
    Signed-off-by: Mayank Grover <groverm@codeaurora.org>

--- kernel/module.c
+++ kernel/module.c
@@ -3909,6 +3909,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	module_disable_nx(mod);
 
  ddebug_cleanup:
+	/* Clean up CFI for the module. */
+	cfi_cleanup(mod);
 	dynamic_debug_remove(mod, info->debug);
 	synchronize_rcu();
 	kfree(mod->args);
