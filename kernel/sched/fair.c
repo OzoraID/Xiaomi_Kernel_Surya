48297447454c986d984805af5112a269cfe5f04a
Alexander Winkowski <dereference23@outlook.com>
2022-03-15 17:34:36 +0000

    sched: Apply Android tweaks manually
    Tunables can't be changed with CONFIG_SCHED_DEBUG=n
    
    https://github.com/LineageOS/android_system_core/blob/b4b3950e528cc62e874e59d052b2e9a600de708a/rootdir/init.rc#L323
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -112,7 +112,7 @@ unsigned int sysctl_sched_cstate_aware = 1;
  *
  * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
  */
-enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
@@ -125,7 +125,7 @@ unsigned int normalized_sysctl_sched_min_granularity	= 400000ULL;
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 6;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
