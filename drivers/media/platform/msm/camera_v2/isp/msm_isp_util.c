e6d28dba9a0297f2a9c6ac2c86774e9544963e74
Richard Raya <rdxzv.dev@gmail.com>
2026-02-18 01:54:55 -0300

    msm: camera: Dynamically handle camera timestamp for recording start time
    Android 11/12 ROMs rely on monotonic timestamps for correct video duration
    calculations, while current ROMs uses the boot-time ktime timestamp.
    
    This patch adds a cmdline flag (init.is_legacy_timestamp) to choose
    the correct timestamp at runtime to effectively support both scenarios.
    
    Change-Id: I64e18519a185e9f73635bdfcea2d41549be2cd7a
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -215,7 +215,10 @@ void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp,
 		time_stamp->buf_time.tv_sec    = time_stamp->vt_time.tv_sec;
 		time_stamp->buf_time.tv_usec   = time_stamp->vt_time.tv_usec;
 	} else {
-		get_monotonic_boottime(&ts);
+		if (is_legacy_timestamp)
+			get_monotonic_boottime(&ts);
+		else
+			ktime_get_ts(&ts);
 		time_stamp->buf_time.tv_sec    = ts.tv_sec;
 		time_stamp->buf_time.tv_usec   = ts.tv_nsec/1000;
 		time_stamp->buf_time_ns        =
