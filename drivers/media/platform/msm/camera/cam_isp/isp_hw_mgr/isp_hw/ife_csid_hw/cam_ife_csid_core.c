f9d04f561180523f7ded4af48f8303e405596c08
NekoTuru10 <kirmansyah9696@gmail.com>
2026-08-12 13:45:42 +0000

    Revert "drivers: camera: Add switch for enable/disable boot clock timestamp"
    This reverts commit 4ffff69c414a1e584f5cb475e95b12bd315576aa.

--- drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
@@ -2584,12 +2584,7 @@ static int cam_ife_csid_get_time_stamp(
 		CAM_IFE_CSID_QTIMER_DIV_FACTOR);
 
 	if (!csid_hw->prev_boot_timestamp) {
-		#ifdef CONFIG_CAMERA_BOOTCLOCK_TIMESTAMP
-			ktime_get_ts64(&ts);
-		#else
-			get_monotonic_boottime64(&ts);
-		#endif
-
+		ktime_get_ts64(&ts);
 		time_stamp->boot_timestamp =
 			(uint64_t)((ts.tv_sec * 1000000000) +
 			ts.tv_nsec);
